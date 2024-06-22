da836eb935612467647a172eb95484aff12db451
logistics <info@sofane.nl>
2024-06-22 03:07:54 +0200

    02-96. Exercise: Your Task: Make /meals, /meals/share, /community, /meals/<some slug>, links

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,6 @@
+export default function MealsPostPage({ params }) {
+  return <main>
+<h1>Meals Post</h1>
+<p>{params.slug}</p>
+  </main>
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function MealsSharePage() {
+  return (
+    <main>
+      <h1>These is the meals-share</h1>
+    </main>
+  )
+}
