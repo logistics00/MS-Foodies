830c5b3f99044d2f1032fae8c16781ec16ef4019
logistics <info@sofane.nl>
2024-06-24 21:25:46 +0200

    09-103. Populating The Starting Page Content: modify page.js and add page.module.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,37 @@
 import Link from 'next/link'
 
+import classes from './page.module.css'
+
 export default function Home() {
   return (
-    <main>
-      <h1>Time to get started!</h1>
-      <p>
-        <Link href='/meals'>Meals</Link>
-      </p>
-      <p>
-        <Link href='/meals/share'>Share Meal</Link>
-      </p>
-      <p>
-        <Link href='/community'>Community</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}></div>
+        <div>
+          <div className={classes.hero}>
+            <h1>Heerlijk Eten voor smulpapen</h1>
+            <p>Recepten binnen de Mollers-familie</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href='/community'>De Mollers gemeenschap</Link>
+            <Link href='/meals'>Recepten</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>Hoe werkt Heerlijk Eten?</h2>
+          <p>Heerlijk Eten is een platform voor smulpapen voor het ontdekken en delen van nieuwe recepten.</p>
+        </section>
+
+        <section className={classes.section}>
+          <h2>Waarom Heerlijk Eten?</h2>
+          <p>
+            Heerlijk Eten is een platform voor smulpapen waarop zij hun favoriete recepten met anderen kunnen delen.
+          </p>
+          <p>Heerlijk Eten is een omgeving om nieuwe gerechten te ontdekken.</p>
+        </section>
+      </main>
+    </>
   )
 }
