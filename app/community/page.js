297bcab4aede46ec748f0e76a902de495c5d2556
logistics <info@sofane.nl>
2024-06-26 03:24:09 +0200

    12-106. Using Client Components Efficiently: add nav-link.js and nav-link.module.css in /main-header

--- app/community/page.js
+++ app/community/page.js
@@ -1,7 +1,37 @@
+import Image from 'next/image'
+
+import mealIcon from '@/assets/icons/meal.png'
+import communityIcon from '@/assets/icons/community.png'
+import eventsIcon from '@/assets/icons/events.png'
+import classes from './page.module.css'
+
 export default function CommunityPage() {
   return (
-    <main>
-      <h1>Community</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Een gedeelde passie: <span className={classes.highlight}>Eten</span>
+        </h1>
+        <p>Sluit je aan bij onze community en deel je favoriete recepten!</p>
+      </header>
+      <main className={classes.main}>
+        <h2>Voordelen van de community</h2>
+
+        <ul className={classes.perks}>
+          <li>
+            <Image src={mealIcon} alt='A delicious meal' />
+            <p>Deel en ontdek recepten</p>
+          </li>
+          <li>
+            <Image src={communityIcon} alt='A crowd of people, cooking' />
+            <p>Vind gelijkgestemden</p>
+          </li>
+          <li>
+            <Image src={eventsIcon} alt='A crowd of people at a cooking event' />
+            <p>Neem deel aan exclusieve evenementen</p>
+          </li>
+        </ul>
+      </main>
+    </>
   )
 }
--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 import MainHeaderBackground from './main-header-background'
+import NavLink from './nav-link'
 import logoImg from '@/assets/logo.png'
 import classes from './main-header.module.css'
 
@@ -17,10 +18,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href='/meals'>Browse Meals</Link>
+              <NavLink href='/meals'>Browse Meals</NavLink>
             </li>
             <li>
-              <Link href='/community'>Community</Link>
+              <NavLink href='/community'>Community</NavLink>
             </li>
           </ul>
         </nav>
