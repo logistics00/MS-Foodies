ff022c7f23109da9d5784630d38d7f7a228966d3
logistics <info@sofane.nl>
2024-06-27 02:32:07 +0200

    13-107. Outputting Meals Data & Images With Unknown Dimensions: add meals-grid and meal-item

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,24 @@
 import Link from 'next/link'
 
+import MealsGrid from '@/components/meals/meals-grid'
+
+import classes from './page.module.css'
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Heerlijke maaltijden, <span className={classes.highlight}>door jullie</span> gecreëerd
+        </h1>
+        <p>Kies je favoriete recept en kook het zelf. Het is gemakkelijk!</p>
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Deel je favoriete recept.</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   )
 }
