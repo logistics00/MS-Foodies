70e9e1668c901996035c3c0bae3660de7ac402ab
logistics <info@sofane.nl>
2024-06-28 20:00:00 +0200

    19-112. handling Errors: Add error.js

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Error() {
+  return (
+    <main className='error'>
+      <h1>Er is een fout geconstateerd!</h1>
+      <p>Maaltijdgegevens kunnen niet worden gevonden! Wellicht later!</p>
+    </main>
+  )
+}
