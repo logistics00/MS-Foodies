0e4e68b3d02d51630bf1625f9f571c530d01853f
logistics <info@sofane.nl>
2024-06-30 02:40:41 +0200

    26-119 Improving the Image Picker Component: add prop required to the input-element

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -58,6 +58,7 @@ export default function ImagePicker({ label, name }) {
           name={name}
           ref={imageInput}
           onChange={handleImageChange}
+          required
         />
         <button className={classes.nutton} type='button' onClick={handlePickClick}>
           Pick an Image
